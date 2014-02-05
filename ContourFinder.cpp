871f018dcb8aa0d15705fff4cf88ad6c0f9d1148
H. Cole Wiley <hcwiley@gmail.com>
2014-02-04 21:35:38 -0600

    trying to implement some calculus... @mcous check this shit out

--- ContourFinder.cpp
+++ ContourFinder.cpp
@@ -116,8 +116,7 @@ void newTemplateImage(cv::Mat * templateMat, cv::Mat * grayMat, cv::Mat * differ
   areas.clear();
   templateContours.clear();
   viewer.removeAllPointClouds();
-  for( int i = contours.size() - 1; i >= 0; i-- )
-  {
+  for( int i = contours.size() - 1; i >= 0; i-- ) {
     bool contains = false;
     if( contourArea(contours[i]) > min_contour_area){
       for( int j = 0; j< origins.size(); j++ ){
@@ -152,157 +151,171 @@ void newTemplateImage(cv::Mat * templateMat, cv::Mat * grayMat, cv::Mat * differ
       areas.push_back(area0);
       templateContour[0].insert(templateContour[0].end(), contours[i].begin(), contours[i].end());
       num_over_min_area++;
-      std::cout << "Creating Point Cloud..." <<std::endl;
-      //pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ>);
-      pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ>), 
-                                      cloud_filtered (new pcl::PointCloud<pcl::PointXYZ>), 
-                                      cloud_projected (new pcl::PointCloud<pcl::PointXYZ>);
-      for(int p = 0; p < contours[i].size(); p++){
-        //Insert info into point cloud structure
-        if( contourArea(contours[i]) > 21000 ) {
-          pcl::PointXYZ point;
-          point.x = contours[i][p].x;
-          point.y = contours[i][p].y;
-          point.z = 0;
-          cloud->points.push_back (point);
-        }
-        else if( contourArea(contours[i]) > 11000 ) {
-          pcl::PointXYZ point;
-          point.x = contours[i][p].x;
-          point.y = contours[i][p].y;
-          point.z = 20;
-          cloud->points.push_back (point);
-          point.x = contours[i][p].x;
-          point.y = contours[i][p].y;
-          point.z = -20;
-          cloud->points.push_back (point);
-        }
-        else if( contourArea(contours[i]) > 1000 ) {
-          pcl::PointXYZ point;
-          point.x = contours[i][p].x;
-          point.y = contours[i][p].y;
-          point.z = 40;
-          cloud->points.push_back (point);
-          point.x = contours[i][p].x;
-          point.y = contours[i][p].y;
-          point.z = -40;
-          cloud->points.push_back (point);
-        }
-      }
-      pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> handler (cloud, 255 * i, 0, 105);
-      char name[20];
-      //sprintf(name, "volume%d", volumeCount);
-      //viewer.addPointCloud<pcl::PointXYZ> (cloud, handler, name);
-      
-      pcl::PassThrough<pcl::PointXYZ> pass;
-      pass.setInputCloud (cloud);
-      pass.setFilterFieldName ("z");
-      pass.setFilterLimits (-45, 45.1);
-      pass.filter (*cloud_filtered);
-      std::cerr << "PointCloud after filtering has: "
-                << cloud_filtered->points.size () << " data points." << std::endl;
-    
-      pcl::ModelCoefficients::Ptr coefficients (new pcl::ModelCoefficients);
-      pcl::PointIndices::Ptr inliers (new pcl::PointIndices);
-      // Create the segmentation object
-      pcl::SACSegmentation<pcl::PointXYZ> seg;
-      // Optional
-      seg.setOptimizeCoefficients (true);
-      // Mandatory
-      seg.setModelType (pcl::SACMODEL_PLANE);
-      seg.setMethodType (pcl::SAC_RANSAC);
-      seg.setDistanceThreshold (100000000.01);
-    
-      seg.setInputCloud (cloud_filtered);
-      seg.segment (*inliers, *coefficients);
-      std::cerr << "PointCloud after segmentation has: "
-                << inliers->indices.size () << " inliers." << std::endl;
-    
-      // Project the model inliers
-      pcl::ProjectInliers<pcl::PointXYZ> proj;
-      proj.setModelType (pcl::SACMODEL_PLANE);
-      proj.setIndices (inliers);
-      proj.setInputCloud (cloud_filtered);
-      proj.setModelCoefficients (coefficients);
-      proj.filter (*cloud_projected);
-      std::cerr << "PointCloud after projection has: "
-                << cloud_projected->points.size () << " data points." << std::endl;
-    
-      // Create a Concave Hull representation of the projected inliers
-      pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_hull (new pcl::PointCloud<pcl::PointXYZ>);
-      pcl::ConcaveHull<pcl::PointXYZ> chull;
-      chull.setInputCloud (cloud_projected);
-      chull.setAlpha (0.8);
-      chull.reconstruct (*cloud_hull);
-    
-      std::cerr << "Concave hull has: " << cloud_hull->points.size ()
-                << " data points." << std::endl;
-
-      pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> handler_hull (cloud_hull, 0, 255, 0);
-      sprintf(name, "vol-hull%d", volumeCount);
-      viewer.addPointCloud<pcl::PointXYZ> (cloud_hull, handler_hull, name);
-          
-
-      // Normal estimation*
-      pcl::NormalEstimation<pcl::PointXYZ, pcl::Normal> n;
-      pcl::PointCloud<pcl::Normal>::Ptr normals (new pcl::PointCloud<pcl::Normal>);
-      pcl::search::KdTree<pcl::PointXYZ>::Ptr tree (new pcl::search::KdTree<pcl::PointXYZ>);
-      tree->setInputCloud (cloud);
-      n.setInputCloud (cloud);
-      n.setSearchMethod (tree);
-      n.setKSearch (200000000);
-      n.compute (*normals);
-      //* normals should not contain the point normals + surface curvatures
-      
-      // Concatenate the XYZ and normal fields*
-      pcl::PointCloud<pcl::PointNormal>::Ptr cloud_with_normals (new pcl::PointCloud<pcl::PointNormal>);
-      pcl::concatenateFields (*cloud, *normals, *cloud_with_normals);
-      //* cloud_with_normals = cloud + normals
-      
-      // Create search tree*
-      pcl::search::KdTree<pcl::PointNormal>::Ptr tree2 (new pcl::search::KdTree<pcl::PointNormal>);
-      tree2->setInputCloud (cloud_with_normals);
-      
-      // Initialize objects
-      pcl::GreedyProjectionTriangulation<pcl::PointNormal> gp3;
-      pcl::PolygonMesh triangles;
-      
-      // Set the maximum distance between connected points (maximum edge length)
-      gp3.setSearchRadius (1000000.025);
-      
-      // Set typical values for the parameters
-      gp3.setMu (200000.5);
-      gp3.setMaximumNearestNeighbors (1000000);
-      gp3.setMaximumSurfaceAngle(M_PI/8); // 45 degrees
-      gp3.setMinimumAngle(M_PI/36); // 10 degrees
-      gp3.setMaximumAngle(M_PI/3); // 120 degrees
-      gp3.setNormalConsistency(false);
-      
-      // Get result
-      gp3.setInputCloud (cloud_with_normals);
-      gp3.setSearchMethod (tree2);
-      gp3.reconstruct (triangles);
-      
-      // Additional vertex information
-      std::vector<int> parts = gp3.getPartIDs();
-      std::vector<int> states = gp3.getPointStates();
-
-      std::cerr << "Shape has: " << parts.size ()
-                << " parts." << std::endl;
-
-      sprintf(name, "shape-%d", volumeCount++);
-      viewer.addPolygonMesh(triangles, name, 0);
-
     }
   }
+  std::cout << "Creating Point Cloud..." <<std::endl;
+  //pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ>);
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ>), 
+                                  cloud_filtered (new pcl::PointCloud<pcl::PointXYZ>), 
+                                  cloud_projected (new pcl::PointCloud<pcl::PointXYZ>);
+
+  double scale = 1000/(SCALE_REF); // 1946 is a magic number that needs to be derived programmatically eventually
+  int min_x, min_y, max_x, max_y;
+  min_x = min_y =  1000000;
+  max_x = max_y = -1000000;
+  for( int i = templateContours.size() - 1; i >= 0; i-- ) {
+    if( contourArea(templateContours[i]) < 2100 ) {
+      continue;
+    }
+    for(int p = 0; p < templateContours[i].size(); p++){
+      //Insert info into point cloud structure
+      if( contourArea(templateContours[i]) > 21000 ) {
+        pcl::PointXYZ point;
+        point.x = templateContours[i][p].x;
+        point.y = templateContours[i][p].y;
+        point.z = 0;
+        cloud->points.push_back (point);
+        if(point.x > max_x)
+          max_x = point.x;
+        else if(point.x < min_x)
+          min_x = point.x;
+        if(point.y > max_y)
+          max_y = point.y;
+        else if(point.y < min_y)
+          min_y = point.y;
+      }
+      else if( contourArea(templateContours[i]) > 8500 ) {
+        pcl::PointXYZ point;
+        point.x = templateContours[i][p].x;
+        point.y = templateContours[i][p].y;
+        point.z = 20;
+        cloud->points.push_back (point);
+        point.x = templateContours[i][p].x;
+        point.y = templateContours[i][p].y;
+        point.z = -20;
+        cloud->points.push_back (point);
+      }
+      else {
+        pcl::PointXYZ point;
+        point.x = templateContours[i][p].x;
+        point.y = templateContours[i][p].y;
+        point.z = 40;
+        cloud->points.push_back (point);
+        point.x = templateContours[i][p].x;
+        point.y = templateContours[i][p].y;
+        point.z = -40;
+        cloud->points.push_back (point);
+      }
+    }
+  }
+  printf("min (%d, %d)\nmax (%d, %d)\n", min_x,min_y,max_x,max_y);
+  // loop through between min and max x and y
+  for( int x = min_x; x < max_x; x+=5 ) {
+    for( int y = min_y; y < max_y; y+=5 ) {
+      // loop through all the contours
+      for( int i = templateContours.size() - 1; i >= 0; i-- ) {
+        // if the point is in the contour, lets do it
+        if( pointPolygonTest(templateContours[i], cv::Point(x,y), false) > 0 ) {
+          // if its in the main area
+          if( contourArea(templateContours[i]) > 21000 ) {
+            pcl::PointXYZ point;
+            point.x = x;
+            point.y = y;
+            point.z = 0;
+            cloud->points.push_back (point);
+          }
+          // if its in the middle sizes
+          else if( contourArea(templateContours[i]) > 8500 ) {
+            pcl::PointXYZ point;
+            point.x = x;
+            point.y = y;
+            point.z = 20;
+            cloud->points.push_back (point);
+            point.x = x;
+            point.y = y;
+            point.z = -20;
+            cloud->points.push_back (point);
+          }
+          // other wise its a small one
+          else{
+            pcl::PointXYZ point;
+            point.x = x;
+            point.y = y;
+            point.z = 40;
+            cloud->points.push_back (point);
+            point.x = x;
+            point.y = y;
+            point.z = -40;
+            cloud->points.push_back (point);
+          }
+        }
+      }
+    }
+  }
+  printf("done adding extra points\n");
+  char name[20];
+  pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> handler (cloud, 0, 0, 255);
+  sprintf(name, "volume%d", volumeCount);
+  viewer.addPointCloud<pcl::PointXYZ> (cloud, handler, name);
+  
+  // Normal estimation*
+  pcl::NormalEstimation<pcl::PointXYZ, pcl::Normal> n;
+  pcl::PointCloud<pcl::Normal>::Ptr normals (new pcl::PointCloud<pcl::Normal>);
+  pcl::search::KdTree<pcl::PointXYZ>::Ptr tree (new pcl::search::KdTree<pcl::PointXYZ>);
+  tree->setInputCloud (cloud);
+  n.setInputCloud (cloud);
+  n.setSearchMethod (tree);
+  n.setKSearch (21);
+  n.compute (*normals);
+  //* normals should not contain the point normals + surface curvatures
+  
+  // Concatenate the XYZ and normal fields*
+  pcl::PointCloud<pcl::PointNormal>::Ptr cloud_with_normals (new pcl::PointCloud<pcl::PointNormal>);
+  pcl::concatenateFields (*cloud, *normals, *cloud_with_normals);
+  //* cloud_with_normals = cloud + normals
+  
+  // Create search tree*
+  pcl::search::KdTree<pcl::PointNormal>::Ptr tree2 (new pcl::search::KdTree<pcl::PointNormal>);
+  tree2->setInputCloud (cloud_with_normals);
+  
+  // Initialize objects
+  pcl::GreedyProjectionTriangulation<pcl::PointNormal> gp3;
+  pcl::PolygonMesh triangles;
+  
+  // Set the maximum distance between connected points (maximum edge length)
+  gp3.setSearchRadius (10);
+  
+  // Set typical values for the parameters
+  gp3.setMu (3.5);
+  gp3.setMaximumNearestNeighbors (21);
+  gp3.setMaximumSurfaceAngle(M_PI/2); // 45 degrees
+  gp3.setMinimumAngle(M_PI/36); // 10 degrees
+  gp3.setMaximumAngle(M_PI/2); // 120 degrees
+  gp3.setNormalConsistency(false);
+  
+  // Get result
+  gp3.setInputCloud (cloud_with_normals);
+  gp3.setSearchMethod (tree2);
+  gp3.reconstruct (triangles);
+  
+  // Additional vertex information
+  std::vector<int> parts = gp3.getPartIDs();
+  std::vector<int> states = gp3.getPointStates();
+
+  std::cerr << "Shape has: " << parts.size ()
+            << " parts." << std::endl;
+
+  sprintf(name, "shape-%d", volumeCount++);
+  viewer.addPolygonMesh(triangles, name, 0);
+
   //cv::convexHull(templateContour[0], templateContour[0], false);
   cv::Scalar color = cv::Scalar( 0, 0, 255);
   drawContours( *differenceMat, templateContour, 0, color, 2, 8, hierarchy, 0, cv::Point() );
   viewer.addCoordinateSystem(1.0,0);
 
+  scale = 1000*1000/(SCALE_REF); // 1946 is a magic number that needs to be derived programmatically eventually
   for( int j = 0; j< origins.size(); j++ ){
     char str[40];
-    double scale = 1000*1000/(SCALE_REF); // 1946 is a magic number that needs to be derived programmatically eventually
     double area0 = areas[j];
     area0 *= scale; // to sq ft
     area0 /= 43560; // to acres
@@ -353,22 +366,14 @@ int main( int argc, const char** argv )
 
   IplImage	*	img		= NULL;
   IplImage	*	gray	= NULL;
-  //IplImage	*	edges	= NULL;
   IplImage	*	difference = NULL;
   IplImage	*	templateImg = NULL;
-  //float			angle	= 0.0;
   cv::Scalar lowerThresh = cv::Scalar(0,0,0);
   cv::Scalar upperThresh = cv::Scalar(45,45,45);
-  //cv::Moments momentsTemplate = 0;
-  //cv::Moments momentsLive = 0;
-  //
   cv::vector<cv::Point> foo;
   foo.push_back(cv::Point());
   templateContour.push_back(foo);
   liveContour.push_back(foo);
-
-  //initSocket();
-  //templateContour = cv::vector<cv::vector<cv::Point> >;
 
   bool run = true;
 
@@ -377,35 +382,21 @@ int main( int argc, const char** argv )
 
 
 
-  //capture = cvCaptureFromCAM( 0 ); //0=default, -1=any camera, 1..99=your camera
-  //if(!capture) cout << "No camera detected" << endl;
-
-  //cvSetCaptureProperty ( capture, CV_CAP_PROP_FRAME_WIDTH, 640 );
-  //cvSetCaptureProperty ( capture, CV_CAP_PROP_FRAME_HEIGHT, 480 );
-
-  //width	= (int) cvGetCaptureProperty ( capture, CV_CAP_PROP_FRAME_WIDTH );
-  //height	= (int) cvGetCaptureProperty ( capture, CV_CAP_PROP_FRAME_HEIGHT );
   width = 640;
   height = 480;
 
   cvNamedWindow( "live", 1 );
-  //cvNamedWindow( "gray", 1 );
-  //cvNamedWindow( "edges", 1 );
   cvNamedWindow( "difference", 1 );
-  //cvNamedWindow( "template", 1 );
-
-  //viewer.setSize (800, 600);
 
   cv::resizeWindow("live", 640, 480);//50, 50);
   cv::resizeWindow("template", 640, 480);//700, 50);
   cv::resizeWindow("difference", 640, 480);// 700, 500);
-  //cv::resizeWindow("gray", 640, 480);// 50, 500);
 
   cv::moveWindow("live", 450, 50);
   cv::moveWindow("template", 1400, 50);
   cv::moveWindow("difference", 1400, 50);
-  //cv::moveWindow("gray", 50, 500);
-  //
+
+  // click handler binding
   cv::setMouseCallback("live", liveWindowClickCallback, NULL);
 
   gray = cvCreateImage ( cvSize ( width, height ), IPL_DEPTH_8U, 1 );
@@ -415,10 +406,6 @@ int main( int argc, const char** argv )
   }
 
   img = cvCreateImage ( cvSize ( width, height ), IPL_DEPTH_8U, 1 );
-  //if ( !edges ) {
-  //printf ( "Failed to create edges image!!!\n" );
-  //exit(-1);
-  //}
 
   templateImg = cvCreateImage ( cvSize ( width, height ), IPL_DEPTH_8U, 1 );
   if ( !templateImg ) {
@@ -432,7 +419,6 @@ int main( int argc, const char** argv )
     exit(-1);
   }
 
-  //
   // Set Region of Interest to the whole image
   cvResetImageROI ( gray );
   gray->origin = 1;
@@ -447,23 +433,11 @@ int main( int argc, const char** argv )
 
       double timestamp = (double)clock()/CLOCKS_PER_SEC; // get current time in seconds
 
-      //img = cvQueryFrame( capture );
-      //frame = img;
-
       if( frame.empty() )
         break;
-      //if( img->origin == IPL_ORIGIN_TL )
-      //frame.copyTo( frameCopy );
-      //else
-      //flip( frame, frameCopy, 0 );
-
-
-      //cv::Mat differenceMat(frame);
 
       cv::inRange(frame, lowerThresh, upperThresh, grayMat);
 
-
-      //GaussianBlur(frame, frame, cv::Size(7,7), 1.5, 1.5);
 
       TContours contours;
       std::vector<cv::Vec4i> hierarchy;
@@ -471,46 +445,12 @@ int main( int argc, const char** argv )
       cv::Mat canny_output;//  = cv::Mat::zeros( frame.size(), CV_8UC3 );
 
       Canny(grayMat, canny_output, 10, 50, 3);
-      //Canny(grayMat, canny_output, 1, 100, 3);
-
-      //cv::findContours( canny_output, contours, hierarchy, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE, cv::Point(0, 0) );
-      //cv::findContours( canny_output, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, cv::Point(0, 0) );
-
-      // Print number of found contours.
-      //std::cout << "Found " << contours.size() << " contours." << std::endl;
-
-
-      /// Draw contours
-      //int first = contours.size() + 1;
-      //for( int i = 0; i< contours.size(); i++ )
-      //{
-      //if( contourArea(contours[i]) > min_contour_area){
-      //if ( first > i){
-      //liveContour.clear();
-      //liveContour.push_back(contours[i]);
-      //first = i;
-      //}
-      //liveContour[0].insert(liveContour[0].end(), contours[i].begin(), contours[i].end());
-      //cv::Scalar color = cv::Scalar(50*i,0,0);
-      //drawContours( frame, contours, 0, color, 2, 8, hierarchy, 0, cv::Point() );
-      //}
-      //}
-
-      //cv::convexHull(liveContour[0], liveContour[0], false);
-
-      //cvtColor (frame, grayMat, CV_BGR2GRAY );
       grayMat= frame.clone();
 
       *gray = grayMat;
       if(templateMat.empty() ){
         newTemplateImage(&templateMat, &grayMat, &differenceMat);
       }
-
-      //if(!templateContour.empty()){
-      //drawContours( frame, templateContour, 0, cv::Scalar(0,255,0), 2, 8, hierarchy, 0, cv::Point() );
-      //double area0 = contourArea(liveContour);
-      //printf("contour: %f\n", area0);
-      //}
 
 
       int key = cv::waitKey( 10 );
@@ -520,7 +460,6 @@ int main( int argc, const char** argv )
             run = false;
             break;
           case T_KEY:
-            //newTemplateImage(&templateMat, &grayMat);
             newTemplateImage(&templateMat, &grayMat, &differenceMat);
             break;
           case UP_KEY:
